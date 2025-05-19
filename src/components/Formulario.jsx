9d8e16bedf42dc8cf192c2e1cb900e0d59d45de5
AriJusid <48042387@est.ort.edu.ar>
2025-05-19 12:10:31 -0300

    Supuesto avance

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,34 +1,58 @@
-import React from "react";
+import {React, useState} from "react";
 import './Form.css'
 import Button from "./Button";
-
+export default Formulario
 function Formulario ({setCitas}) {
+   
+    const [cita, setValor] = useState({
+        nombreMascota: '',
+        nombre: '',
+        fecha:'',
+        hora: '',
+        sintomas: ''
+    });
 
-    const guardarValor = (cita) =>{
-        
-    }
-    const agregarCita = (cita) => {
+    const guardarValor = (cita) => {
         const citaAgregada = {
-          id: Date.now(),
           nombreMascota: cita.nombre,
           nombre: cita.dueño,
           fecha: cita.fecha,
           hora: cita.hora,
           sintomas: cita.sintomas
         };
-        setCitas([...citas, citaAgregada]);
+        return citaAgregada
+    }
     
+       
+    const agregarCita = (citaAgregada) =>{
+        if (Object.values(citaAgregada).every(valor => valor !== null)){
+            setCitas(() => [...cita, setValor])
+            
+            setValor({
+                nombreMascota: '',
+                nombreDueño: '',
+                fecha: '',
+                hora: '',
+                sintomas: ''
+              });
+        }       
+
+        else{
+            alert("Por favor completa todos los campos.");
+        }
+    }
+
     return(
         <>  
             <div className="one-half column">
 
-            <form  onSubmit = { () => setCitas(agregarCita(citaAgregada))} >
+            <form  onSubmit = { () => agregarCita()} >
             <h2>Crear mi Cita</h2>
-                <label>Nombre Mascota</label><input type="text" value="mascota" className="u-full-width" placeholder="Nombre Mascota" id="nombreM" onChange={guardarValor()}/>
-                <label>Nombre Dueño</label><input type="text" value="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" id="dueño" onChange={guardarValor()}/>
-                <label>Fecha</label><input type="date" value="fecha" className="u-full-width" id = "date"  onChange={guardarValor()}/>
-                <label>Hora</label><input type="time" value="hora" className="u-full-width" id ="time"  onChange={guardarValor()}/>
-                <label>Sintomas</label><textarea value="sintomas" className="u-full-width" id ="sintomas"  onChange={guardarValor()}></textarea>
+                <label>Nombre Mascota</label><input type="text" value={cita.nombre}  className="u-full-width" placeholder="Nombre Mascota" id="nombre" onChange={guardarValor()}/>
+                <label>Nombre Dueño</label><input type="text" value={cita.dueño} className="u-full-width" placeholder="Nombre dueño de la mascota" id="dueño" onChange={guardarValor()}/>
+                <label>Fecha</label><input type="date" value={cita.fecha} className="u-full-width" id = "date"  onChange={guardarValor()}/>
+                <label>Hora</label><input type="time" value={cita.hora} className="u-full-width" id ="time"  onChange={guardarValor()}/>
+                <label>Sintomas</label><textarea value={cita.sintomas} className="u-full-width" id ="sintomas"  onChange={guardarValor()}></textarea>
                 <Button/>
             </form>
             </div>
@@ -37,4 +61,4 @@ function Formulario ({setCitas}) {
     );
 }
 
-export default Formulario
+
